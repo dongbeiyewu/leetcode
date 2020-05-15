bbdd8b957119340ccf73b93216896377183b4952
qiming sun <sunqiming@bupt.edu.cn>
2020-05-15 22:03:03 +0800

    1

---
+++ hash相关/[q1_两数之和/1.两数之和.py
@@ -0,0 +1,21 @@
+#
+# @lc app=leetcode.cn id=1 lang=python
+#
+# [1] 两数之和
+#
+
+# @lc code=start
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        for i in range(0,len(nums)):
+            for j in range(i+1,len(nums)):
+                if (nums[i]+nums[j]==target):
+                    return [i ,j]
+                
+# @lc code=end
+
